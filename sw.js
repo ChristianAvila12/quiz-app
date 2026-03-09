71407f5252d22c06b2942752accbee438e845e41
ChristianAvila12 <avilachristian232321@gmail.com>
2026-03-09 14:21:16 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,40 @@
-self.addEventListener('install', event => {
+
+// Name of the cache
+const CACHE_NAME = "quiz-app-cache-v1";
+
+// Files to cache
+const urlsToCache = [
+  "/",             // root
+  "/index.html",   // your main HTML
+  "/icon.png",
+];
+
+// Install event: cache files
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open('quiz-cache').then(cache => {
-      return cache.addAll([
-        '/index.html',
-        '/manifest.json',
-        '/icon.png',
-        '/icon.png'
-      ]);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
     })
   );
 });
 
-self.addEventListener('fetch', event => {
+// Fetch event: serve cached files if offline
+self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
       return response || fetch(event.request);
+    })
+  );
+});
+
+// Activate event: cleanup old caches
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME)
+                  .map(name => caches.delete(name))
+      );
     })
   );
 });
